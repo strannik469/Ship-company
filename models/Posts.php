fd3a685ee39b78fede710c5c5376bb8238c0420f
Blackened <Blackened>
2016-05-23 21:26:13 +0300

    add Posts.php

--- models/Posts.php
+++ models/Posts.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace app\models;
+
+class Posts extends \yii\db\ActiveRecord{
+	
+	public static function tableName() {
+		
+		return 'tblPosts';
+	}
+	
+	public static function getAll() {
+		
+		$data = self::find()->all();
+		
+		return $data;
+	}
+	
+	public static function getOne($id) {
+		
+		$data = self::find()->where(['id'=>$id])
+		->one();
+		
+		return $data;
+	}
+	
+}
