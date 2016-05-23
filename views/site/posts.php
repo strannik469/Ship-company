bd3ddd073a714e7ddce75cd26becf6db48f1a025
Blackened <Blackened>
2016-05-23 21:37:34 +0300

    add posts.php

--- views/site/posts.php
+++ views/site/posts.php
@@ -1,1 +1,12 @@
+<h1> <?php echo $varInView; ?> </h1>
 
+<div>
+	
+		<?php foreach ($arrayInView as $item): ?>
+			<p> <a href="/site/view/<?=$item->id?>">
+			<?php echo $item->post;  ?>  </a>
+			
+			</p>
+		<?php endforeach ?>	
+	
+</div>
