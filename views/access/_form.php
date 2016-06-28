6f1a42cf213fa0d61fefd7cd2f8470221ea87e21
Blackened <Blackened>
2016-06-28 18:57:57 +0300

    _form.php

--- views/access/_form.php
+++ views/access/_form.php
@@ -1,1 +1,31 @@
+<?php
 
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Access */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="access-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'idType')->textInput() ?>
+
+    <?= $form->field($model, 'dateStart')->textInput() ?>
+
+    <?= $form->field($model, 'dateEnd')->textInput() ?>
+
+    <?= $form->field($model, 'number')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'idWorkers')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
