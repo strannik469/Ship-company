83232268173fe537ef49205ed6582eb8e5b1803f
Blackened <Blackened>
2016-06-28 18:46:34 +0300

    Posts.php

--- models/Posts.php
+++ models/Posts.php
@@ -2,26 +2,18 @@
 
 namespace app\models;
 
-class Posts extends \yii\db\ActiveRecord{
-	
-	public static function tableName() {
-		
+use yii\db\ActiveRecord;
+
+class Posts extends ActiveRecord
+{
+	public static function tableName()
+	{
 		return 'tblPosts';
 	}
-	
-	public static function getAll() {
-		
-		$data = self::find()->all();
-		
-		return $data;
+	public function rules()
+	{
+		return [
+		    ['post', 'required'],
+		];
 	}
-	
-	public static function getOne($id) {
-		
-		$data = self::find()->where(['id'=>$id])
-		->one();
-		
-		return $data;
-	}
-	
 }
